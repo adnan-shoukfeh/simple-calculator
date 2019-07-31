476ee3b6cc9a06998a8662c37f1c8a8811cf1660
Adnan Shoukfeh <sadnan@umich.edu>
2019-07-30 23:14:47 -0400

    Basic layout with no functionality (yet)

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Adnan Shoukfeh on 7/30/19.
+//  Copyright © 2019 Adnan Shoukfeh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
