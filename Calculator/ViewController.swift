2268df7471d3e345c9d135c55770db6f8bcd7ac0
Adnan Shoukfeh <sadnan@umich.edu>
2019-08-01 23:45:23 -0400

    Started adding some functionality

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,12 +9,90 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var temp = String()
+    var valueOne = Double()
+    var valueTwo = Double()
+    var valueTotal = Double()
+    
+    @IBOutlet var valueOutputted: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
-
+    func updateOutput() {
+        valueOutputted.text = temp
+    }
+    
+    @IBAction func decimalPressed(_ sender: UIButton) {
+        temp += "."
+        updateOutput()
+    }
+    @IBAction func zeroPressed(_ sender: UIButton) {
+        temp += "0"
+        updateOutput()
+    }
+    
+    @IBAction func onePressed(_ sender: UIButton) {
+        temp += "1"
+        updateOutput()
+    }
+    
+    @IBAction func twoPressed(_ sender: Any) {
+        temp += "2"
+        updateOutput()
+    }
+    
+    @IBAction func threePressed(_ sender: Any) {
+        temp += "3"
+        updateOutput()
+    }
+    
+    @IBAction func fourPressed(_ sender: Any) {
+        temp += "4"
+        updateOutput()
+    }
+    
+    @IBAction func fivePressed(_ sender: Any) {
+        temp += "5"
+        updateOutput()
+    }
+    
+    @IBAction func sixPressed(_ sender: Any) {
+        temp += "6"
+        updateOutput()
+    }
+    
+    @IBAction func sevenPressed(_ sender: Any) {
+        temp += "7"
+        updateOutput()
+    }
+    
+    @IBAction func eightPressed(_ sender: Any) {
+        temp += "8"
+        updateOutput()
+    }
+    
+    @IBAction func ninePressed(_ sender: Any) {
+        temp += "9"
+        updateOutput()
+    }
+    
+    @IBAction func changeSign(_ sender: Any) {
+        if temp.contains("-") {
+            temp.remove(at: temp.startIndex)
+        } else {
+            temp = "-" + temp
+        }
+        updateOutput()
+    }
+    
+    @IBAction func modPressed(_ sender: Any) {
+        var tempNum = Double(temp)
+        tempNum = tempNum! / 100
+        temp = "\(tempNum ?? 0)"
+        updateOutput()
+    }
 }
 
